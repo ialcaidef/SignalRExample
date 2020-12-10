7c375a85eb45ee00d3b61d0240987400dddd3936
Alcaide F. Inocente <ialcaidef@hotmail.com>
2020-12-10 21:14:13 +0100

    Add project files.

---
+++ SignalRExample/Hubs/SquaresHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using SignalRExample.Services;
+using SignalRExample.Hubs;
+
+namespace SignalRExample.Hubs
+{
+    public class SquaresHub : Hub
+    {
+        private ISquareManager _manager;
+
+        public SquaresHub(ISquareManager manager)
+        {
+            _manager = manager;
+        }
+
+        public async Task SwapColor(int rowIndex, int columnIndex)
+        {
+            _manager.SwapColor(rowIndex, columnIndex);
+            await Clients.Others.SendAsync("SwapSquareColor", rowIndex, columnIndex);
+        }
+    }
+}
